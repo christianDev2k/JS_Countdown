abda901591406bb9fb48a17fe60a8225186cdc98
Hoang Nhan <hnhan793@gmail.com>
2023-05-13 22:17:09 +0700

    add expired event

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const christmasDay = new Date('2023-12-25');
 const halloweenDay = new Date('2023-10-31');
 const valentineDay = new Date('2024-02-14');
 const countDownBg = document.querySelector('.countdown');
+const showExpired = document.querySelector('#showExpired');
 
 function countDown(day) {
     // Khởi tạo về 00h00p00s mặc định 07h00p00s
@@ -20,27 +21,34 @@ function countDown(day) {
     const x = setInterval(function () {
         let currentDay = new Date().getTime();
         let count = eventDay - currentDay;
-
-        // Điều kiện dừng 
-        count--;
-        count < 0 ? clearInterval(x) : null;
+        
+        // Điều kiện dừng và show kết quả
+        count -= 1000;
+        let showResult = document.querySelectorAll('.showDay');
+        if (count <= 0) {
+            clearInterval(x);
+            showExpired.innerText = 'EVENT EXPIRED';
+            showResult.forEach(function (item) {
+                item.innerHTML = '---';
+            })
+        }
         selectDay.addEventListener('change', function () {
             clearInterval(x);
-        })
-
-        let time = [];
+        });
 
         // Quy đổi thời gian từ mili giây - ngày / giờ / phút / giây
+        let time = [];
         time[0] = Math.floor(count / 86400000);
         time[1] = Math.floor(count % 86400000 / 3600000);
         time[2] = Math.floor(count % 3600000 / 60000);
         time[3] = Math.floor(count % 60000 / 1000);
 
         // Inner vào HTML 
-        let showResult = document.querySelectorAll('.showDay');
-        showResult.forEach(function (item, index) {
-            item.innerHTML = time[index];
-        })
+        if (count > 0) {
+            showResult.forEach(function (item, index) {
+                item.innerHTML = time[index];
+            });
+        }
     }, 1000);
 }
 
